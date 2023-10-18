9a823494302efc25d21974f19b7473dc1655e6d9
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-18 21:01:02 +0545

    Note that the images are set to static/images/recipe

--- core/core/settings.py
+++ core/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,9 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# FOR HANDLING STATIC AND MEDIA FILES
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[BASE_DIR,'static']
 
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
