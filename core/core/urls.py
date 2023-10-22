b9353036f6eef5cc341e64399ed6c38351d7cc5b
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-22 17:01:23 +0545

    updated the data through update form

--- core/core/urls.py
+++ core/core/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('contact/',contact,name="contact"),
     path('about/',about,name="about"),
     path('receipes/',receipes,name="receipes"),
-    path('delete_receipe/<id>/',delete_receipe,name="delete_receipe")
+    path('delete_receipe/<id>/',delete_receipe,name="delete_receipe"),
+    path('update_receipe/<id>/',update_receipe,name="update_receipe"),
 ]
 
 if settings.DEBUG:
